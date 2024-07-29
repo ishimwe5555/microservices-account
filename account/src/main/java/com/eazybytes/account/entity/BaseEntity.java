0aabc461bcd618ee75da949fd9ef03a4f86c5ca3
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-29 15:47:45 +0200

    Account, adds BaseEntry

--- account/src/main/java/com/eazybytes/account/entity/BaseEntity.java
+++ account/src/main/java/com/eazybytes/account/entity/BaseEntity.java
@@ -1,7 +1,16 @@
 package com.eazybytes.account.entity;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
 public class BaseEntity {
 
     private LocalDateTime createdAt;
