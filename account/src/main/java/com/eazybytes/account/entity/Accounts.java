f7bdb3b3b45a04e5dbaa72746e5d93d250767480
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-29 18:48:32 +0200

    Account, adds Accounts and Customer

---
+++ account/src/main/java/com/eazybytes/account/entity/Accounts.java
@@ -0,0 +1,9 @@
+package com.eazybytes.account.entity;
+
+public class Accounts extends BaseEntity{
+
+    private Long customerId;
+    private Long accountNumber;
+    private String accountType;
+    private String branchAddress;
+}
