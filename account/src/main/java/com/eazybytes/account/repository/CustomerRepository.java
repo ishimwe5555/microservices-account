9ecd115c41e02a0c9efc220aed15665147dcdc27
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-29 19:29:51 +0200

    Account, adds AccountsDto

--- account/src/main/java/com/eazybytes/account/repository/CustomerRepository.java
+++ account/src/main/java/com/eazybytes/account/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.eazybytes.account.repository;
 
 import com.eazybytes.account.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
