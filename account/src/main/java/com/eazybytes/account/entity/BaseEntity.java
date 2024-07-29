861d94327b5ac104512e8a89785b4b965f11742c
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-29 12:01:43 +0200

    Account, adds BaseEntiry

---
+++ account/src/main/java/com/eazybytes/account/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.eazybytes.account.entity;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updateAt;
+}
