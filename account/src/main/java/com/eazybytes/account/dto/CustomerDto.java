758b43f4142526d37d972bf8bca6920a3e6fa306
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-29 19:31:11 +0200

    Account, adds CustomerDto

---
+++ account/src/main/java/com/eazybytes/account/dto/CustomerDto.java
@@ -0,0 +1,11 @@
+package com.eazybytes.account.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
