49aab2350f96ed3495ad35ee3c332ed4a0c9fbf8
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-30 13:21:27 +0200

    Account, Implements fetchAccountDetails

--- account/src/main/java/com/eazybytes/account/controller/AccountsController.java
+++ account/src/main/java/com/eazybytes/account/controller/AccountsController.java
@@ -8,22 +8,27 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 public class AccountsController {
 
-    private IAccountsService iAccountsService;
-    @PostMapping("create")
+    private final IAccountsService iAccountsService;
+    @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto body){
         iAccountsService.createAccount(body);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
+        CustomerDto response = iAccountsService.fetchAccount(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
