088d43e60b625f0c4f8047e279e1e6faf799224f
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-30 10:19:56 +0200

    Account, adds CustomerAlreadyExistsException

--- account/src/main/java/com/eazybytes/account/service/impl/AccountsServiceImpl.java
+++ account/src/main/java/com/eazybytes/account/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,17 @@
 package com.eazybytes.account.service.impl;
 
+import com.eazybytes.account.constant.AccountsConstants;
 import com.eazybytes.account.dto.CustomerDto;
+import com.eazybytes.account.entity.Accounts;
+import com.eazybytes.account.entity.Customer;
+import com.eazybytes.account.mapper.CustomerMapper;
 import com.eazybytes.account.repository.AccountRepository;
 import com.eazybytes.account.repository.CustomerRepository;
 import com.eazybytes.account.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +20,19 @@ public class AccountsServiceImpl implements IAccountsService {
     private CustomerRepository customerRepository;
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);
+        accountRepository.save(createNewAccount(savedCustomer));
+    }
 
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
     }
 }
