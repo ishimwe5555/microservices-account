8a761465c75ec230655c6e895923fd65d54451a6
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-30 15:29:03 +0200

    Account, AuditAwareImpl

---
+++ account/src/main/java/com/eazybytes/account/audit/AuditAwareImpl.java
@@ -0,0 +1,12 @@
+package com.eazybytes.account.audit;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNT_MS");
+    }
+}
