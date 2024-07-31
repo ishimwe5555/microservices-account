b9c9c7e573a19ed795c607a1e455b34e887c775b
Norbert Ishimwe <ishimwe96@gmail.com>
2024-07-31 13:04:29 +0200

    Account, Adds documentation endpoints description

--- account/src/main/java/com/eazybytes/account/controller/AccountsController.java
+++ account/src/main/java/com/eazybytes/account/controller/AccountsController.java
@@ -4,6 +4,8 @@ import com.eazybytes.account.constant.AccountsConstants;
 import com.eazybytes.account.dto.CustomerDto;
 import com.eazybytes.account.dto.ResponseDto;
 import com.eazybytes.account.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -25,6 +27,14 @@ import org.springframework.web.bind.annotation.*;
 public class AccountsController {
 
     private final IAccountsService iAccountsService;
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create a new Customer & Account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status Created"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto body){
         iAccountsService.createAccount(body);
@@ -33,6 +43,14 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Account details REST API",
+            description = "REST API to fetch an Account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
                                                                @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
@@ -43,6 +61,14 @@ public class AccountsController {
                 .body(response);
     }
 
+    @Operation(
+            summary = "Update Account details REST API",
+            description = "REST API to update an Account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
@@ -57,6 +83,14 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account details REST API",
+            description = "REST API to delete an Account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
                                                                 @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
