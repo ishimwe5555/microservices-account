fde8366eb2cafdabc13da6e539647f76749cd24f
Norbert Ishimwe <ishimwe96@gmail.com>
2024-08-07 14:14:31 +0200

    configserver,encrypts values in application.yml

--- loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,14 @@
 package com.eazybytes.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
+@Getter
+@Setter
 public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
 }
