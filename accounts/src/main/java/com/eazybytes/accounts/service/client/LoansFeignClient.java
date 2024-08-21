14968d134615a866fea968a9a253d0073f73c792
Norbert Ishimwe <ishimwe96@gmail.com>
2024-08-21 11:28:44 +0200

    Accounts, implement feinClient

--- accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -1,13 +1,13 @@
 package com.eazybytes.accounts.service.client;
 
-import com.eazybytes.accounts.dto.CardsDto;
+import com.eazybytes.accounts.dto.LoansDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("cards")
-public interface CardsFeignClient {
+public interface LoansFeignClient {
     @GetMapping(value = "api/fetch", consumes = "application/json")
-    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);
 }
