e04750fcf29244d21a0731827dc39a850b4007c2
Norbert Ishimwe <ishimwe96@gmail.com>
2024-08-23 14:20:51 +0200

    gateway, implements resilience

--- accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFallBack.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFallBack.java
@@ -2,7 +2,9 @@ package com.eazybytes.accounts.service.client;
 
 import com.eazybytes.accounts.dto.LoansDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoansFallBack implements LoansFeignClient{
     @Override
     public ResponseEntity<LoansDto> fetchLoanDetails(String mobileNumber) {
