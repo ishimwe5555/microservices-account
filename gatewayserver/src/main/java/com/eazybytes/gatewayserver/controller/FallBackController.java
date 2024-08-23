6d39ffbc9ab11de4eadf862fa2f44a72a91ff589
Norbert Ishimwe <ishimwe96@gmail.com>
2024-08-23 12:52:28 +0200

    gateway, implements resilience

---
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/controller/FallBackController.java
@@ -0,0 +1,12 @@
+package com.eazybytes.gatewayserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallBackController {
+
+    public Mono<String> contactSupport() {
+        return Mono.just("An error occurred. Please try after some time or contact support team");
+    }
+}
