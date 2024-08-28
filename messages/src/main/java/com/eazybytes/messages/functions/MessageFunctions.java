7d4501f227655c156b1dfabed940e3482dd0b14d
Norbert Ishimwe <ishimwe96@gmail.com>
2024-08-28 14:13:36 +0200

    messages, initialize message broker

--- messages/src/main/java/com/eazybytes/messages/functions/MessageFunctions.java
+++ messages/src/main/java/com/eazybytes/messages/functions/MessageFunctions.java
@@ -1,7 +1,20 @@
 package com.eazybytes.messages.functions;
 
+import com.eazybytes.messages.dto.AccountsMsgDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
 
 @Configuration
 public class MessageFunctions {
+    private static final Logger logger = LoggerFactory.getLogger(MessageFunctions.class);
+
+    public Function<AccountsMsgDto, AccountsMsgDto> email(){
+        return accountsMsgDto -> {
+            logger.info("Sending email with details" + accountsMsgDto.toString());
+            return accountsMsgDto;
+        };
+    }
 }
