446574d09b121d9f6239d8a0faefb0757beac919
Norbert Ishimwe <ishimwe96@gmail.com>
2024-08-28 14:58:47 +0200

    messages, initialize message broker

--- messages/src/main/java/com/eazybytes/messages/dto/AccountsMsgDto.java
+++ messages/src/main/java/com/eazybytes/messages/dto/AccountsMsgDto.java
@@ -1,4 +1,4 @@
 package com.eazybytes.messages.dto;
 
-public record AccountsMsgDto(Long accountNumber, String email, String mobileNumber) {
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
 }
