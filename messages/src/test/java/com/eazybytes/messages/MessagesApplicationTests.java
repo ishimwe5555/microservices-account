ddd8c7009b40f5089d3662a969e5913dfc2df8a2
Norbert Ishimwe <ishimwe96@gmail.com>
2024-08-28 14:04:07 +0200

    messages, initialize message broker

---
+++ messages/src/test/java/com/eazybytes/messages/MessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.messages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
